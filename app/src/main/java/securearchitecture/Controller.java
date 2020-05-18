34c890f552f765b69db1f8001492f3e466605802
JuandeMata <matalmagro@gmail.com>
2020-05-18 21:22:22 +0200

    Simple application

---
+++ app/src/main/java/securearchitecture/Controller.java
@@ -0,0 +1,23 @@
+package securearchitecture;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class Controller {
+
+    @Value("${fruitivice.url}")
+    private String url;
+
+    @RequestMapping("/")
+    public String login() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response
+                = restTemplate.getForEntity(url, String.class);
+        return response.getBody();
+    }
+
+}
---
+++ app/src/test/java/securearchitecture/ControllerTest.java
@@ -0,0 +1,14 @@
+package securearchitecture;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+    @Test
+    public void test() {
+        assertNotNull(new Controller());
+    }
+
+}
